36b2681bf47d2dd475a934b3ea60ca2c0d556cb6
ChahrazedBz <chahrazedamina191@gmail.com>
2024-09-09 22:32:43 +0100

    second commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <section className={styles.container}>
     <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Amina</h1>
-            <p className={styles.description}>I'm a full-stack developer with 5 years of experience using React and NodeJs.Reach out if you'd like to learn more!</p>
+            <p className={styles.description}>I'm a full-stack developer with 2 years of experience using React and Django.Reach out if you'd like to learn more!</p>
             <a href="mailto:myemail@mail.com" className={styles.conatctBtn} >Contact me</a>
             </div>
             <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg}></img>
